93bbce7b27f6550c38a7771017a23277ff973b74
JavaEE <JavaEE@CowryCode>
2018-09-10 23:43:50 +0200

    initial

--- src/main/java/com/cowrycode/test/CdiLifeCircle.java
+++ src/main/java/com/cowrycode/test/CdiLifeCircle.java
@@ -5,10 +5,32 @@
  */
 package com.cowrycode.test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+
 /**
  *
  * @author JavaEE
  */
+
 public class CdiLifeCircle {
+ 
+    @Inject
+    Logger logger;
+    
+    @PostConstruct
+    private void  init(){
+        logger.log(Level.SEVERE, "****** Initialized*******");
+    }
+    
+    @PreDestroy
+    private void kill(){
+        logger.log(Level.SEVERE, "*********** End of bean life");
+    }
+    
+    
     
 }
